013a906fcb18698e03515a3adc2f64ac49008ea7
Igor Todorovski <itodorov@ca.ibm.com>
2023-10-16 10:49:36 -0400

    Update buildenv/groovy

---
+++ cicd-dev.groovy
@@ -0,0 +1,28 @@
+
+node('linux')
+{
+  stage ('Poll') {
+               // Poll from upstream:
+               checkout([
+                                               $class: 'GitSCM',: 'GitSCM',
+                       branches: [[name: '*/...']],
+                       doGenerateSubmoduleConfigurations: false,
+                       extensions: [],
+                       userRemoteConfigs: [[url: 'https://...']]])
+
+                // Poll for local changes
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/opensshport.git"]]])
+  }
+
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/opensshport.git'), string(name: 'PORT_DESCRIPTION', value: 'OpenSSH' )]
+
+  string(name: 'BUILD_LINE', value: 'DEV')]
+  }
+}
+
---
+++ cicd-stable.groovy
@@ -0,0 +1,20 @@
+
+node('linux')
+{
+  stage ('Poll') {
+                // Poll for local changes
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/opensshport.git"]]])
+  }
+
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/opensshport.git'), string(name: 'PORT_DESCRIPTION', value: 'OpenSSH' )]
+
+  string(name: 'BUILD_LINE', value: 'STABLE')]
+  }
+}
+
